7014b5d4cfc506a7d34243c0f274b5fd0b43481d
Sakshi Nair <131491465+sakshinair27@users.noreply.github.com>
2026-08-13 14:52:07 -0400

    Delete models directory

--- models/stg_categories.sql
+++
@@ -1,9 +0,0 @@
-with source as (
-    select * from {{ source('raw', 'categories_raw') }}
-)
-
-select
-    category_id,
-    category_name,
-    nullif(parent_category_id, '') as parent_category_id
-from source
